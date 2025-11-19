9da6132f939a0b870e00aa660c547fa01a012d3e
HopeLotriet <hopelotriet@gmail.com>
2025-11-19 21:23:24 +0200

    Media changes

--- core/settings.py
+++ core/settings.py
@@ -256,11 +256,19 @@ DATABASES = {
     }
 }
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Media files
 
-# 🚨 TEMPORARY DEMO CHANGE - Create media folder on startup
-os.makedirs(MEDIA_ROOT, exist_ok=True)
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'chieta-media'
+AWS_S3_REGION_NAME = 'us-west-2'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+# Default file storage to S3
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://chieta-media.s3.amazonaws.com/'
+
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
